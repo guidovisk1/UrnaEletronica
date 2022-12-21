2fd7dc7cf58660806796fac3ce238ca21785703a
guidovisk1 <fehlipecardooso@gmail.com>
2022-12-21 11:15:28 -0300

    testes

---
+++ UrnaEletronica.Tests/ValueObjects/NomeTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using UrnaEletronica.Domains.ValueObjects;
+using Xunit;
+
+namespace UrnaEletronica.Tests.ValueObjects
+{
+    public sealed class NomeTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("Ab")]
+        [InlineData("     ")]
+
+        public void DeveRetornar_Erro_Quando_PrimeiroNome_EhInvalido(string primeiroNome)
+        {
+            var name = new Nome(primeiroNome, "Messi");
+
+            Assert.False(name.IsValid);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("Abc")]
+        [InlineData("     ")]
+        public void DeveRetornar_Erro_Quando_UltimoNome_EhInvalido(string sobrenome)
+        {
+            var name = new Nome("Lionel", sobrenome);
+
+            Assert.False(name.IsValid);
+        }
+
+    }
+}
---
+++ UrnaEletronica.Tests/ValueObjects/PartidoTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrnaEletronica.Domains.ValueObjects;
+using Xunit;
+
+namespace UrnaEletronica.Tests.ValueObjects
+{
+    public sealed class PartidoTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("Ab")]
+
+        public void DeveRetornar_Erro_Quando_NomePartido_EhInvalido(string nomePartido)
+        {
+            var name = new Partido(nomePartido,new DateOnly(2005,10,11),"pl" );
+
+            Assert.True(name.IsValid);
+        }
+
+    }
+}
