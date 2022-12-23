8ba60a0f620770deb083e1377fa6162be82e058d
FelipePox <felipe.cardoso@go-it.work>
2022-12-23 09:50:12 -0300

    services

---
+++ UrnaEletronica.Application/Services/CandidatoServices.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrnaEletronica.Application.Interfaces;
+using UrnaEletronica.Application.ViewModel;
+using UrnaEletronica.Domains.Interfaces;
+
+namespace UrnaEletronica.Application.Services
+{
+    public class CandidatoServices : ICandidatoService
+    {
+        private readonly ICandidatoRepository _candidatoRepository;
+        private readonly IMapper _mapper;
+        public CandidatoServices(ICandidatoRepository candidatoRepository, IMapper mapper)
+        {
+            _candidatoRepository = candidatoRepository;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<CandidatoViewModel>> GetAllAsync()
+        {
+            return _mapper.Map<IEnumerable<CandidatoViewModel>>(await _candidatoRepository.GetAllAsync());
+        }
+    }
+}
---
+++ UrnaEletronica.Application/ViewModel/CandidatoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrnaEletronica.Domains.ValueObjects;
+
+namespace UrnaEletronica.Application.ViewModel
+{
+   public class CandidatoViewModel
+    {
+        public Guid Id { get; private set; }
+        public Nome Nome { get; private set; }
+        public Idade Idade { get; private set; }
+        public Partido Partido { get; private set; }
+    }
+}
